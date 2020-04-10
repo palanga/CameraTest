61a28610661633b869935975fecb026216e255ab
Andrés González <a.gonzalez.terres@gmail.com>
2020-04-10 09:36:15 +0200

    Initial Commit

---
+++ CameraTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CameraTest
+//
+//  Created by Andrés González on 10/04/2020.
+//  Copyright © 2020 Andrés González. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
