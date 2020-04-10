8c012afaebb2616a4aec4599d23d83f3ce11ca76
Andrés González <a.gonzalez.terres@gmail.com>
2020-04-10 15:45:43 +0200

    preview funcionando

--- CameraTest/ContentView.swift
+++ CameraTest/ContentView.swift
@@ -7,10 +7,18 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    
+    let camera: Camera = CameraBuilder().make()!
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Text("hola")
+            DemoVideoStreaming()
+            
+        }
     }
 }
 
@@ -18,4 +26,86 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+
+struct DemoVideoStreaming: View {
+    var body: some View {
+        VStack {
+            PreviewHolder()
+        }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+    }
+}
+
+
+
+class PreviewView: UIView {
+    private var captureSession: AVCaptureSession?
+    
+    init() {
+        super.init(frame: .zero)
+        
+        var allowedAccess = false
+        let blocker = DispatchGroup()
+        blocker.enter()
+        AVCaptureDevice.requestAccess(for: .video) { flag in
+            allowedAccess = flag
+            blocker.leave()
+        }
+        blocker.wait()
+        
+        if !allowedAccess {
+            print("!!! NO ACCESS TO CAMERA")
+            return
+        }
+        
+        // setup session
+        let session = AVCaptureSession()
+        session.beginConfiguration()
+        
+        let videoDevice = AVCaptureDevice.default(.builtInTelephotoCamera, for: .video, position: .unspecified)
+        guard videoDevice != nil, let videoDeviceInput = try? AVCaptureDeviceInput(device: videoDevice!), session.canAddInput(videoDeviceInput) else {
+            print("!!! NO CAMERA DETECTED")
+            return
+        }
+        session.addInput(videoDeviceInput)
+        session.commitConfiguration()
+        self.captureSession = session
+    }
+    
+    override class var layerClass: AnyClass {
+        AVCaptureVideoPreviewLayer.self
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    var videoPreviewLayer: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+    
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        
+        if nil != self.superview {
+            self.videoPreviewLayer.session = self.captureSession
+            self.videoPreviewLayer.videoGravity = .resizeAspect
+            self.captureSession?.startRunning()
+        } else {
+            self.captureSession?.stopRunning()
+        }
+    }
+}
+
+
+struct PreviewHolder: UIViewRepresentable {
+    func makeUIView(context: UIViewRepresentableContext<PreviewHolder>) -> PreviewView {
+        PreviewView()
+    }
+    
+    func updateUIView(_ uiView: PreviewView, context: UIViewRepresentableContext<PreviewHolder>) {
+    }
+    
+    typealias UIViewType = PreviewView
 }
